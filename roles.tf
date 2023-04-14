781df24b736cf942573107196d50a463df9415de
saipranayteja <pranayteja.ch@algonox.com>
2023-04-14 18:40:35 +0530

    go

--- roles.tf
+++ roles.tf
@@ -1,27 +1,37 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["*"]
-    }
-
-    actions = ["*"]
-  }
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
-
-
 
 resource "aws_iam_role" "role" {
   name               = "dummy_role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "*"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "*"
+        }
+      },
+    ]
+  })
 }
 
-resource "aws_iam_policy" "policy" {
-  name        = "dummy_policy"
-  policy      = data.aws_iam_policy_document.assume_role.json
-}
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
   role       = aws_iam_role.role.name
