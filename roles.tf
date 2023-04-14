f61c57dfcbe811a3fa974d4999dc59b9b4fd79a0
saipranayteja <pranayteja.ch@algonox.com>
2023-04-14 19:00:29 +0530

    no

--- roles.tf
+++ roles.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role_policy" "test_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "*,"
+      "Action": "*",
       "Effect": "Allow",
       "Resource": "*"
     }
