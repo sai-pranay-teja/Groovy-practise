85239665b604de747c4b601b790798fce43b5460
saipranayteja <pranayteja.ch@algonox.com>
2023-04-14 19:14:55 +0530

    go

--- roles.tf
+++ roles.tf
@@ -3,19 +3,20 @@ resource "aws_iam_role" "role" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-        "Effect": "Allow",
-        "Action": "*",
-        "Principal": {
-            "Service": [
-                "ec2.amazonaws.com"
-            ]
-        },
-        "Sid": ""
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
+        }
+    ]
 }
 EOF
 
