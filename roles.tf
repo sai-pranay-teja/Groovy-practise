8c2a49bb68fc4f8980cb0392acd972a3c79c7365
saipranayteja <pranayteja.ch@algonox.com>
2023-04-14 18:16:07 +0530

    go

--- roles.tf
+++ roles.tf
@@ -4,10 +4,10 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Service"
-      identifiers = "*"
+      identifiers = ["*"]
     }
 
-    actions = "*"
+    actions = ["*"]
   }
 }
 
