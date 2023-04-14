0eb23a4d2d02bb9fbfb12a40b3905d991b4cb8c8
saipranayteja <pranayteja.ch@algonox.com>
2023-04-14 20:50:24 +0530

    fo

--- parameters.tf
+++ parameters.tf
@@ -8,12 +8,14 @@ resource "aws_ssm_parameter" "jenkins_user" {
   name  = "jenkins_user"
   type  = "String"
   value = "admin"
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "jenkins_pass" {
   name  = "jenkins_pass"
   type  = "String"
   value = "Pranay1700#"
+  overwrite = true
   
 }
 
