18755ab1ff2e50cd0a1e29c827344aa94e0e0b25
saipranayteja <pranayteja.ch@algonox.com>
2023-04-14 19:01:41 +0530

    fo

--- roles.tf
+++ roles.tf
@@ -37,7 +37,7 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "test-attach" {
   role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
+  policy_arn = aws_iam_role_policy.policy.arn
 }
 
 resource "aws_iam_instance_profile" "demo-profile" {
