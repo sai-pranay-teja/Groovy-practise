c15dba4d0aa68bf5cc912cb31359f1e4837f0519
saipranayteja <pranayteja.ch@algonox.com>
2023-04-14 11:59:19 +0530

    go

--- instances.tf
+++ instances.tf
@@ -1,4 +1,4 @@
-/* provider "aws"{
+provider "aws"{
     region="us-east-1"
 }
 
@@ -25,10 +25,10 @@ provisioner "remote-exec" {
   }
 
   inline = [
-    "sudo labauto ansible",
+    "sudo labauto jenkins",
     ]
   
 }
 
-} */
+}
 
