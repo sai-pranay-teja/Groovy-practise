4a8f0a4c864b487cd0ca5be5def816d0ba85cbce
saipranayteja <pranayteja.ch@algonox.com>
2023-04-15 15:29:34 +0530

    good

--- vars.tf
+++ vars.tf
@@ -1,18 +1,2 @@
-
-
-variable "jobs-folder"{
-    default=["folder_1"]
-}
-
-variable "jobs"{
-    default=[
-        {
-            name="example",
-            folder="folder_1", 
-            repo_url="https://github.com/sai-pranay-teja/Groovy-practise.git"
-        }
-    ]
-}
-
 variable "jobs-folder" {}
 variable "jobs" {}
