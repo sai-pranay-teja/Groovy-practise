553de8f7c5a55995b0c5550658ff0beed5f4831a
saipranayteja <pranayteja.ch@algonox.com>
2023-04-16 22:06:14 +0530

    go

--- jobs.tf
+++ jobs.tf
@@ -22,6 +22,7 @@ resource "jenkins_folder" "example" {
 }
 
 resource "jenkins_job" "example" {
+  depends_on = [jenkins_folder.example]
   count=length(var.jobs)
   name     = lookup(element(var.jobs, count.index), "name", null)
   #folder   = lookup(element(var.jobs, count.index), "folder", null)
