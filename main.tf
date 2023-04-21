09e04cbdb2e5c34c8c35cb46705a27f6e0554612
saipranayteja <pranayteja.ch@algonox.com>
2023-04-21 17:54:57 +0530

    dfg

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ provider "aws" {
     region = "us-east-1"
   
 }
-module "Instance-setup" {
+/* module "Instance-setup" {
     source = "git::https://github.com/sai-pranay-teja/Instance-setup.git"
     
   
-}
+} */
 
 
-/* module "Jenkins-setup"{
+module "Jenkins-setup"{
     
     source = "git::https://github.com/sai-pranay-teja/Jenkins-setup.git"
    
-} */
+}
 
